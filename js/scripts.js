5cbbaa45620985e8f67c84457e6b2d12e82d310c
Jackson Meyer <jackson.meyer7@gmail.com>
2016-08-19 11:36:08 -0700

    will add 'ping' to every array number that is divisible by 3

--- js/scripts.js
+++ js/scripts.js
@@ -2,26 +2,25 @@
 
 var getPingPonged = function(input) {
   if (input <= 0 ) {
-    console.log ("please enter a positive number");
+    console.log ("please enter a positive number!");
   }
   if (input >= 1) {
     console.log("nice number");
    }
-
    var arr1 = [];
-   for (var index = 0; index <= input; index ++) {
+   for (var index = 1; index <= input; index ++) {
      arr1.push(index);
      console.log(arr1);
-   }
-
-   alert(arr1);
- }
-
-
-
-
-
-
+}
+     arr2 = [];
+     arr1.forEach(function(arr) {
+       if(arr % 3 === 0) {
+         arr = "ping";
+}
+    arr2.push(arr);
+ });
+console.log(arr2);
+};
 
 //this is my frontend logic//
 
